80815e4ea736022dcb2b60a4f058c3bb1acd51f7
Sacha Morlat <sachamorlat@gmail.com>
2024-02-29 14:21:54 +0100

    various changes

--- projetDjango/myapp1/views.py
+++ projetDjango/myapp1/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Voyage, Reservation
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseNotAllowed, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 
 
 def index(request):
@@ -19,7 +21,27 @@ def inscription(request):
         form = UserCreationForm()
     return render(request, 'registration/inscription.html', {'form': form})
 
-@login_required    
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Identifiant ou mot de passe incorrect.')
+    return render(request, 'registrattion/login.html')
+
+@login_required
+def user_logout(request):
+    if request.method == 'GET':  # Vérifiez la méthode HTTP
+        logout(request)
+        return redirect('login')  # Rediriger vers la page de connexion après déconnexion
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+
 def rechercher_voyages(request):
     if request.method == 'POST':
         lieu_depart = request.POST.get('lieu_depart')
